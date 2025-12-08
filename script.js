a3c0a6c7cb70fef8b28a8785e3c797fa146fc7a6
vclf-art <vclf@cesar.school>
2025-12-08 19:06:45 -0300

    Update contact email in alert message

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Recurso em JavaScript: botão que mostra uma mensagem de contato
-
 document.addEventListener("DOMContentLoaded", function () {
     const botaoContato = document.getElementById("btn-contato");
 
     if (botaoContato) {
         botaoContato.addEventListener("click", function () {
-            alert("Você pode falar comigo pelo e-mail: seu-email@exemplo.com");
+            alert("Você pode falar comigo pelo e-mail: vclf@cesar.school");
         });
     }
 });
+
